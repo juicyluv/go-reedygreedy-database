060f28a694db06b8a8f647ba36869364be587385
Ilya <57060144+juicyluv@users.noreply.github.com>
2022-07-20 21:20:29 +0300

    issue #14 - Add function to create category (#41)

---
+++ rgdb/create_category.go
@@ -0,0 +1,63 @@
+package rgdb
+
+import (
+	"context"
+	"fmt"
+	"rgdb/rgdberr"
+	"rgdb/rgdbmsg"
+)
+
+//language=PostgreSQL
+const createCategoryQuery = `
+	select 
+		category_id,
+		error
+	from core.create_category(
+	  _invoker_id := $1, 
+	  _name := $2
+	)
+`
+
+func (d *driver) CreateCategory(ctx context.Context, request *rgdbmsg.CreateCategoryRequest) (*int64, error) {
+	row, err := d.pool.Query(
+		ctx,
+		createCategoryQuery,
+
+		request.InvokerId,
+		request.Name,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf(`%w: %v`, rgdberr.ErrInternal, err)
+	}
+
+	defer row.Close()
+
+	if !row.Next() {
+		if err = row.Err(); err != nil {
+			return nil, fmt.Errorf(`%w: %v`, rgdberr.ErrInternal, err)
+		}
+
+		return nil, rgdberr.ErrInternal
+	}
+
+	var (
+		status     []byte
+		categoryId *int64
+	)
+
+	err = row.Scan(
+		&categoryId,
+		&status,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf(`%w: %v`, rgdberr.ErrInternal, err)
+	}
+
+	if err = rgdberr.AnalyzeQueryStatus(status); err != nil {
+		return nil, err
+	}
+
+	return categoryId, nil
+}
