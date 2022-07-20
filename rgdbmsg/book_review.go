47a0bfe6665a9b2345513e82284596767f281b1c
Ilya <57060144+juicyluv@users.noreply.github.com>
2022-07-21 01:30:56 +0300

    issue #60 - Add list book reviews function (#61)

--- rgdbmsg/book_review.go
+++ rgdbmsg/book_review.go
@@ -1,6 +1,28 @@
 package rgdbmsg
 
+import "time"
+
 type (
+	BookReview struct {
+		ReviewId        *int64     `json:"review_id,omitempty"`
+		BookId          *int64     `json:"book_id,omitempty"`
+		BookTitle       *string    `json:"book_title,omitempty"`
+		CreatorId       *int64     `json:"creator_id,omitempty"`
+		CreatorUsername *string    `json:"creator_username,omitempty"`
+		Review          *int16     `json:"review,omitempty"`
+		CreatedAt       *time.Time `json:"created_at,omitempty"`
+		UpdatedAt       *time.Time `json:"updated_at,omitempty"`
+		Comment         *string    `json:"comment,omitempty"`
+	}
+
+	GetBookReviewsRequest struct {
+		BookId   *int64
+		Search   *string
+		PageSize *int
+		Page     *int
+		Sort     []string
+	}
+
 	AddBookReviewRequest struct {
 		InvokerId int64
 		BookId    int64
@@ -20,3 +42,8 @@ type (
 		ReviewId  int64
 	}
 )
+
+func (req *GetBookReviewsRequest) CheckDefaults() {
+	_default(&req.PageSize, 60)
+	_default(&req.Page, 1)
+}
