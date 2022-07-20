b741c56a8c8d5939262e49107fc65a0d6cb4c7db
juicyluv <juicyluv@mail.ru>
2022-07-20 21:27:18 +0300

    issue #16 - Add function to get categories

--- rgdbmsg/category.go
+++ rgdbmsg/category.go
@@ -7,6 +7,7 @@ type (
 		CategoryId *int16     `json:"category_id,omitempty"`
 		Name       *string    `json:"name,omitempty"`
 		CreatedAt  *time.Time `json:"created_at,omitempty"`
+		UpdatedAt  *time.Time `json:"updated_at,omitempty"`
 	}
 
 	GetCategoryRequest struct {
@@ -17,4 +18,16 @@ type (
 		InvokerId int64
 		Name      string
 	}
+
+	GetCategoriesRequest struct {
+		Search   *string
+		PageSize *int
+		Page     *int
+		Sort     []string
+	}
 )
+
+func (req *GetCategoriesRequest) CheckDefaults() {
+	_default(&req.PageSize, 60)
+	_default(&req.Page, 1)
+}
