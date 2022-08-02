01e882662df5e6d4c24f0e7ac75f90e837eda842
juicyluv <juicyluv@mail.ru>
2022-08-03 00:57:56 +0300

    issue #66 - Add function to get promocodes

--- pkg/rgdbmsg/promocode.go
+++ pkg/rgdbmsg/promocode.go
@@ -3,6 +3,18 @@ package rgdbmsg
 import "time"
 
 type (
+	Promocode struct {
+		PromocodeId     *int64     `json:"promocode_id,omitempty"`
+		Promocode       *string    `json:"promocode,omitempty"`
+		Payload         []byte     `json:"payload,omitempty"`
+		UsageCount      *int       `json:"usage_count,omitempty"`
+		CreatorId       *int64     `json:"creator_id,omitempty"`
+		CreatorUsername *string    `json:"creator_username,omitempty"`
+		CreatedAt       *time.Time `json:"created_at,omitempty"`
+		UpdatedAt       *time.Time `json:"updated_at,omitempty"`
+		EndingAt        *time.Time `json:"ending_at,omitempty"`
+	}
+
 	CreatePromocodeRequest struct {
 		InvokerId  int64
 		Promocode  string
@@ -11,8 +23,21 @@ type (
 		EndingAt   *time.Time
 	}
 
+	GetPromocodesRequest struct {
+		UserId   *int64
+		Search   *string
+		PageSize *int
+		Page     *int
+		Sort     []string
+	}
+
 	DeletePromocodeRequest struct {
 		InvokerId   int64
 		PromocodeId int64
 	}
 )
+
+func (req *GetPromocodesRequest) CheckDefaults() {
+	_default(&req.PageSize, 60)
+	_default(&req.Page, 1)
+}
